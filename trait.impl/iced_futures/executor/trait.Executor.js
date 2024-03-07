d6d36a60b148b94066320f5128d752e590e02358
mmstick <mmstick@users.noreply.github.com>
2024-03-07 02:53:17 +0000

    deploy: 422d9126342d94aa8a8a04be0bd76036fdbcf01c

--- trait.impl/iced_futures/executor/trait.Executor.js
+++ trait.impl/iced_futures/executor/trait.Executor.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {
-"cosmic":[["impl <a class=\"trait\" href=\"iced_futures/executor/trait.Executor.html\" title=\"trait iced_futures::executor::Executor\">Executor</a> for <a class=\"struct\" href=\"cosmic/executor/single/struct.Executor.html\" title=\"struct cosmic::executor::single::Executor\">Executor</a>"],["impl <a class=\"trait\" href=\"iced_futures/executor/trait.Executor.html\" title=\"trait iced_futures::executor::Executor\">Executor</a> for <a class=\"struct\" href=\"cosmic/executor/multi/struct.Executor.html\" title=\"struct cosmic::executor::multi::Executor\">Executor</a>"]],
+"cosmic":[["impl <a class=\"trait\" href=\"iced_futures/executor/trait.Executor.html\" title=\"trait iced_futures::executor::Executor\">Executor</a> for <a class=\"struct\" href=\"cosmic/executor/multi/struct.Executor.html\" title=\"struct cosmic::executor::multi::Executor\">Executor</a>"],["impl <a class=\"trait\" href=\"iced_futures/executor/trait.Executor.html\" title=\"trait iced_futures::executor::Executor\">Executor</a> for <a class=\"struct\" href=\"cosmic/executor/single/struct.Executor.html\" title=\"struct cosmic::executor::single::Executor\">Executor</a>"]],
 "iced":[],
 "iced_futures":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
