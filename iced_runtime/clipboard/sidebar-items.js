7b34256c0802e7987c2082ea1256defb48dd1c00
jackpot51 <jackpot51@users.noreply.github.com>
2024-04-10 15:45:28 +0000

    deploy: 62d527b6e539ae4b6e60607971e5f260eb674f90

--- iced_runtime/clipboard/sidebar-items.js
+++ iced_runtime/clipboard/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["Action"],"fn":["read","read_data","read_primary_data","write","write_data","write_primary_data"]};
+window.SIDEBAR_ITEMS = {"enum":["Action"],"fn":["read","read_data","read_primary","read_primary_data","write","write_data","write_primary","write_primary_data"]};
